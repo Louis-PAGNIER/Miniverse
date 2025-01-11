6c66e1a4084c24065ee300a90d6c2a150b825332
Louis <louis.pagnier@epita.fr>
2025-01-11 14:20:31 +0100

    Blob shader effect independent of camera

--- src/components/Blob.vue
+++ src/components/Blob.vue
@@ -7,8 +7,8 @@ const {onLoop} = useRenderLoop()
 
 const uniforms = {
   uTime: {value: 0},
-  uAmplitude: {value: new Vector2(0, 0.14)},
-  uFrequency: {value: new Vector2(7, 7)},
+  uAmplitude: {value: new Vector2(0.15, 0.15)},
+  uFrequency: {value: new Vector2(35, 35)},
 }
 
 const currentScale = shallowRef(1)
@@ -47,25 +47,22 @@ varying vec3 vPosition;
 
 void main() {
     vec3 normal = normalize(vNormal);
-    vec3 viewDir = normalize(cameraPosition - vPosition);
+    vec3 viewDir = vec3(0.0, 0.0, 1.0);
 
     vec3 lightDir = normalize(vec3(5.0, 5.0, 0.0));
-    float diff = max(dot(normal, lightDir), 0.2); // Minimum pour éviter les ombres trop sombres
+    float diff = max(dot(normal, lightDir), 0.2);
 
-    float fresnel = pow(1.0 - dot(viewDir, normal), 2.5);
+    float fresnel = pow(1.0 - dot(viewDir, normal), 2.0);
 
-    float gradient = smoothstep(0.0, 1.0, vUv.y + fresnel * 0.4);
+    float gradient = smoothstep(0.0, 1.0, vUv.y + fresnel * 0.3);
 
-    // Couleurs vibrantes ajustées (violet et vert)
     vec3 color1 = vec3(0.6, 0.2, 1.0); // Violet avec une touche de douceur
     vec3 color2 = vec3(0.2, 1.0, 0.6); // Vert légèrement atténué
     vec3 baseColor = mix(color1, color2, gradient);
 
-    // Combinaison des effets d'éclairage, Fresnel et couleur de base
-    vec3 lighting = baseColor * (0.4 + diff * 0.2) + vec3(fresnel * 1.1);
+    vec3 lighting = baseColor * (0.5 + diff * 0.2) + vec3(fresnel * 1.8);
 
-    // Transparence légèrement augmentée
-    gl_FragColor = vec4(lighting, 0.5);
+    gl_FragColor = vec4(lighting, 0.4);
 }
 `
 
@@ -99,7 +96,7 @@ const handleMouseLeave = () => {
       @pointer-leave="handleMouseLeave"
   >
 
-    <TresSphereGeometry :args="[4, 16, 16]"/>
+    <TresSphereGeometry :args="[4, 28, 28]"/>
     <TresShaderMaterial :vertexShader="vertexShader" :fragmentShader="fragmentShader" :uniforms="uniforms" transparent/>
 
     <slot></slot>
