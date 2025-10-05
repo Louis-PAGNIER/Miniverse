cdae1c3a17aa7d100dd9a75c81ac4e2816549dc0
Louis Pagnier <louis14.pagnier@gmail.com>
2025-10-05 22:50:48 +0200

    Add: Players list page

---
+++ src/components/FlatPlayerHead.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps<{
+  username: string;
+}>();
+
+const MINESKIN_BASE_URL = "https://mineskin.eu/helm/";
+const skinUrl = computed(() => `${MINESKIN_BASE_URL}/${props.username}`);
+</script>
+
+<template>
+  <img :src="skinUrl" :alt="`${username} player head`" />
+</template>
+
+<style scoped>
+img {
+  aspect-ratio: 1 / 1;
+  image-rendering: pixelated;
+  border-radius: 15%;
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
--- src/components/MiniverseSheet/PlayersSheetPage.vue
+++ src/components/MiniverseSheet/PlayersSheetPage.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import {computed, inject} from "vue";
+import {computed, ComputedRef, inject} from "vue";
 import {Miniverse} from "@/models/miniverse";
 import {useMiniverseStore} from "@/stores/miniverseStore";
+import FlatPlayerHead from "@/components/FlatPlayerHead.vue";
+import {Player} from "@/models/player";
 
 const miniverse = inject<Miniverse>('miniverse')!;
 const miniverseStore = useMiniverseStore();
 
-const players = computed(() => {
+const players: ComputedRef<Player[]> = computed(() => {
   const animators = miniverseStore.miniversePlayersLists.get(miniverse.id) || [];
   return animators.map((p) => p.player);
 });
@@ -20,12 +22,66 @@ const players = computed(() => {
     </div>
     <ul v-else>
       <li v-for="player in players" :key="player.id">
-        {{ player.name }} ({{ player.id }})
+        <FlatPlayerHead :username="player.name" />
+        <div class="text-info">
+          <span class="player-name">{{ player.name }}</span>
+          <span class="player-role">Operator</span>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <style scoped>
+ul {
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
 
+li {
+  list-style: none;
+  margin-bottom: 10px;
+  width: 100%;
+  height: 80px;
+  background: #3c3c3c;
+  padding: 8px;
+  border-radius: 10px;
+  transition: background 0.2s;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+
+  &:hover {
+    background: #484848;
+  }
+
+  .text-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 5px 0;
+
+    .player-name {
+      font-size: 1.5em;
+      line-height: 1em;
+    }
+
+    .player-role {
+      font-size: 1.15em;
+      color: #b5b5b5;
+      line-height: 1em;
+    }
+  }
+}
+
+h2 {
+  font-size: 1.75em;
+  margin: 0;
+  padding: 10px 0 0 0;
+  font-weight: 500;
+}
 </style>
