e05a4c7e4775a40d81bd6c69f8eb39f986d056b5
Louis Pagnier <louis14.pagnier@gmail.com>
2025-12-30 18:43:41 +0100

    fix: split dev & prod imports for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,51 @@
-import { fileURLToPath, URL } from 'node:url'
-import Pages from "vite-plugin-pages"
-
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-import { templateCompilerOptions } from '@tresjs/core'
+import Pages from 'vite-plugin-pages'
+import {templateCompilerOptions} from '@tresjs/core'
 import mkcert from 'vite-plugin-mkcert'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(async ({ mode }) => {
+  const isDev = mode === 'development'
+
+  const plugins = [
     vue({
       ...templateCompilerOptions,
     }),
     Pages(),
-    vueDevTools(),
-    mkcert(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
+  ]
+
+  if (isDev) {
+    const { default: vueDevTools } = await import('vite-plugin-vue-devtools')
+    // @ts-ignore
+    plugins.push(vueDevTools())
+    // @ts-ignore
+    plugins.push(mkcert())
+  }
+
+  return {
+    plugins,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/ws': {
+          target: 'ws://localhost:8000',
+          changeOrigin: true,
+          ws: true,
+        },
       },
-      '/ws': {
-        target: 'ws://localhost:8000',
-        changeOrigin: true,
-        ws: true
-      }
-    }
-  },
-  preview: {
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
+    preview: {
+      port: 5173,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
